d5d2157f032812cb3a479c1c45ad6cac8353284b
Ernar2002 <erjan.ernar2002@gmail.com>
2022-05-06 01:48:38 +0600

    Method get products by categygory added

--- web/src/main/java/com/shop/onlineshop/controller/api/ApiMainController.java
+++ web/src/main/java/com/shop/onlineshop/controller/api/ApiMainController.java
@@ -1,14 +1,18 @@
 package com.shop.onlineshop.controller.api;
 
+import com.shop.onlineshop.model.entities.Category;
 import com.shop.onlineshop.model.entities.Product;
 import com.shop.onlineshop.service.CategoryService;
 import com.shop.onlineshop.service.ProductService;
 import com.shop.onlineshop.service.UserService;
+import com.shop.onlineshop.utils.CategoryDto;
 import com.shop.onlineshop.utils.ProductDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,9 +30,33 @@ public class ApiMainController {
     @Autowired
     private ProductService productService;
 
+    @GetMapping("/get-categories")
+    public List<CategoryDto> getCategories(){
+        List<Category> categories = categoryService.listCategory();
+        List<CategoryDto> categoryDtoList = new ArrayList<>();
+
+        for(Category category: categories){
+            categoryDtoList.add(CategoryDto.fromCategory(category));
+        }
+
+        return categoryDtoList;
+    }
+
     @GetMapping("/get-products")
     public List<ProductDto> getProducts() {
         List<Product> productList = productService.listProduct();
+        List<ProductDto> productDtoList = new ArrayList<>();
+
+        for(Product product: productList){
+            productDtoList.add(ProductDto.fromProduct(product));
+        }
+
+        return productDtoList;
+    }
+
+    @GetMapping("get-products/{categoryId}")
+    public List<ProductDto> getProductFromCategory(@PathVariable("categoryId") Long categoryId) {
+        List<Product> productList = productService.findByCategory(categoryId);
         List<ProductDto> productDtoList = new ArrayList<>();
 
         for(Product product: productList){
--- web/src/main/java/com/shop/onlineshop/utils/CategoryDto.java
+++ web/src/main/java/com/shop/onlineshop/utils/CategoryDto.java
@@ -1,2 +1,21 @@
-package com.shop.onlineshop.utils;public class CategoryDto {
+package com.shop.onlineshop.utils;
+
+import com.shop.onlineshop.model.entities.Category;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor @NoArgsConstructor
+public class CategoryDto {
+    private Long categoryId;
+    private String name;
+
+    public static CategoryDto fromCategory(Category category){
+        CategoryDto categoryDto = new CategoryDto();
+        categoryDto.setCategoryId(category.getCategoryId());
+        categoryDto.setName(category.getCategoryName());
+
+        return categoryDto;
+    }
 }
